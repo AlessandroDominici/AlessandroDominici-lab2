4be82a328f2466c12ae405b34ecc0a7b60e1fab9
AlessandroDominici <ale7dominici@gmail.com>
2024-10-06 19:51:02 -0400

    Update DotChaser.java
    commit my changes from vscode

--- DotChaser.java
+++ DotChaser.java
@@ -1,138 +1,45 @@
-import java.util.*;
+import java.util.Random;
 
+// responsible for running simulation
 public class DotChaser {
-  public static Random rand = new Random(System.currentTimeMillis());
 
-  /**
-   * A "Thing" moves in a grid world. A TypeA Thing randomly
-   * decides to turn left or right (or not turn) every "round",
-   * and, afterward, takes a step forward.  A TypeB Thing
-   * only considers making a random turn every 10th round.
-   *
-   * A STATIC CLASS? OH NO! GET IT OUT OF HERE!
-   */
-  public static class Thing {
-    // dir: 0=North, 1=East, 2=South, 3=West.
-    // timeSinceLast: this is only important for "TypeB" Things.
-    public int  row, col, dir, timeSinceLast;
-    public char lab = 'r';
-    public boolean isTypeB;
-  }
+    // global random object 
+    public static Random rand = new Random(System.currentTimeMillis());
 
-  /**
-   * YOU'LL NEED TO PUT THIS SOMEWHERE ELSE
-   * HINT: WOULDN'T IT BE NICE TO HAVE A LIST OR QUEUE SO THAT
-   *       WE DON'T HAVE TO USE NODES HERE?
-   * This class is for linked lists of Thing's
-   */
-  public static class Node {
-    public Thing data;
-    public Node  next;
-  }
+    // main entrance instead of everything here, the functionality is broken into well defined classes
+    public static void main(String[] args) {
+        ThingList list = new ThingList();
+        int N = 200;
 
-  // EEEEEK! STATIC METHODS!!! PLEASE FIND THEM A BETTER HOME.
-  public static void rightTurn(Thing t) {
-    t.dir = (t.dir + 1) % 4;
-  }
-
-  public static void leftTurn(Thing t) {
-    t.dir = (t.dir + 3) % 4;
-  }
-
-  public static void maybeTurn(Thing t) {
-    int i = rand.nextInt(3);
-
-    if (t.isTypeB) {
-      t.timeSinceLast++;
-
-      if (t.timeSinceLast == 10) {
-        t.timeSinceLast = 0;
-
-        if (i == 1) {
-          rightTurn(t);
+        // allow number of rounds to be set by command line arguments
+        if (args.length != 0) {
+            N = Integer.parseInt(args[0]);
         }
+        
+        // track rounds
+        int count = 0;
+        
+        // infinite simulation loop
+        while (true) {
+            // Every N rounds add new Type a b c objects to the ThingList
+            if (count % N == 0) {
+                list.addThing(new TypeA(45, 50));  // Add TypeA
+                list.addThing(new TypeB(55, 50));  // Add TypeB
+            }
 
-        if (i == 2) {
-          leftTurn(t);
+            // Every 2N rounds, add a new TypeC (Yellow) Thing
+            if (count % (2 * N) == 0) {
+              list.addThing(new TypeC(50, 50));  // Adds a yellow TypeC Thing
+            }
+
+            // prints positons and moves all based on behavior
+            list.printAll();  
+            list.moveAll(rand);   
+
+            // increment round counter 
+            count++;
         }
-      }
-    } else   {
-      if (i == 1) {
-        rightTurn(t);
-      }
-
-      if (i == 2) {
-        leftTurn(t);
-      }
     }
-  }
-
-  public static void step(Thing t) {
-    final int[] dc = {
-      0, 1, 0, -1
-    }, dr = {
-      1, 0, -1, 0
-    };
-    t.row += dr[t.dir];
-    t.col += dc[t.dir];
-  }
-
-  
-  /**
-   * This static method is ok :)
-   */
-  public static void main(String[] args) {
-    int N = 200;
-
-    if( args.length != 0 )
-      N = Integer.parseInt(args[0]);
-
-    // INSTEAD OF A NODE, CREATE SOMETHING MORE USER-FRIENDLY.
-    Node L = null;
-    int count = 0;
-
-    while( true ) {
-      // Every N rounds, add another typeA and typeB Thing.
-      if( count % N == 0 ) {
-
-        // Add a typeA thing to the list.
-        // (GEE, THAT'S A LOT OF CODE FOR JUST CREATING ONE THING)
-        Thing tA = new Thing();
-        tA.row = 45;
-        tA.col = 50;
-        Node nA = new Node();
-        nA.data = tA;
-        nA.next = L;
-        L       = nA;
-
-        // Add a typeB thing to the list
-        Thing tB = new Thing();
-        tB.row     = 55;
-        tB.col     = 50;
-        tB.lab     = 'b';
-        tB.isTypeB = true;
-        Node nB = new Node();
-        nB.data = tB;
-        nB.next = L;
-        L       = nB;
-      }
-
-      // Print out each thing.
-      // (SEEMS LIKE A NICE PRINTALL() METHOD CALL WOULD WORK HERE)
-      // (SEEMS LIKE A toString() METHOD IN THE CLASS WOULD ALSO BE NICE)
-      for( Node T = L; T != null; T = T.next )
-        System.out.println(T.data.row + " " + T.data.col + " " + T.data.lab);
-
-      System.out.println("done");
-      System.out.flush();
-
-      // Move each thing.
-      // (SEEMS LIKE A NICE MOVEALL() METHOD CALL WOULD WORK HERE)
-      for( Node T = L; T != null; T = T.next ) {
-        maybeTurn(T.data);
-        step(T.data);
-      }
-      count++;
-    }
-  }
 }
+
+
