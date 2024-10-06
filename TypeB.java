4aa93109e19bfc130e003bdf511389118b97bf07
AlessandroDominici <ale7dominici@gmail.com>
2024-10-06 19:53:02 -0400

    Update TypeB.java
    add type B

--- TypeB.java
+++ TypeB.java
@@ -1,3 +1,33 @@
-public class TypeB {
-    
+import java.util.Random;  
+
+// inherits from Thing
+public class TypeB extends Thing {
+    private int timeSinceLast;
+
+    // constructor calls parent thing class 
+    public TypeB(int row, int col) {
+        // Type B Blue label 
+        super(row, col, 'b');  
+        this.timeSinceLast = 0;
+    }
+
+    @Override
+    public void maybeTurn(Random rand) {
+        // increment count to track
+        timeSinceLast++;
+        // turn every ten rounds
+        if (timeSinceLast == 10) {
+            // random number between 0 and 2
+            int i = rand.nextInt(3);
+            timeSinceLast = 0;
+            // right if i is 1
+            if (i == 1) {
+                rightTurn();
+            // left if i is 2
+            } else if (i == 2) {
+                leftTurn();
+            }
+            // if i is 0 no turn
+        }
+    }
 }
